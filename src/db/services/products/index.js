699bba2f477df431ec97ce473beaef2098302168
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-11-03 16:02:18 +0000

    Update product routes

--- src/db/services/products/index.js
+++ src/db/services/products/index.js
@@ -1,5 +1,7 @@
 import express from "express";
+import models from "../../../db/models/index.js"
 
+const { Product, Review } = models
 const router = express.Router()
 
 router
@@ -12,20 +14,38 @@ router
             next(error)
         }
     })
-    .post(() => {
-
+    .post(async (req,res, next) => {
+        try {
+            const data = await Product.create(req.body)
+            res.send(data)
+        } catch (error) {
+            next(error)
+        }
     })
 
 router
     .route("/:id")
-    .get(() => {
-
+    .get(async (req, res, next) => {
+        try {
+           const product = await Product.findByPk(req.params.id)
+           res.send(product) 
+        } catch (error) {
+            next(error)
+        }
     }) 
-    .put(() => {
-
+    .put(async (req, res, next) => {
+        try {
+            const updatedProduct = await Product.update(req.body, {
+                where: {
+                    id: req.params.id,
+                },
+                returning: true,
+            })
+            res.send(updatedProduct[1][0])
+        } catch (error) {
+            next(error)
+        }
     }) 
-    .delete(() => {
-
-    }) 
+    .delete(() => {}) 
 
 export default router;
